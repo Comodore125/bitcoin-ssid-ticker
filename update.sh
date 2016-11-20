2caa1c991ee09b4eb88a21106c094c4c78c6b26d
Nadav Ivgi <nadav@shesek.info>
2016-11-20 09:39:57 +0200

    initial commit

---
+++ update.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+TICKER='📈 1 bitcoin = %P (%C)\n📈 1 BTC = %P (%C)\n📈 1 Bitcoin = %P (%C)\n📈 bitcoin = %P (%C)\n📈 BTC = %P (%C)\n📈 Bitcoin = %P (%C)'
+INTERVAL=15
+IFACE=wlan0
+SSIDPATH=/tmp/bitcoin-ticker-ssid.txt
+
+iw dev $IFACE interface add mon0 type monitor && ifconfig mon0 up
+
+echo . > $SSIDPATH
+mdk3 mon0 b -s 500 -m -a -c 11 -h -f $SSIDPATH &
+
+while :
+do
+  echo "Updating bitcoin ticker..."
+  TICKER="$TICKER" ./ticker.sh | tee $SSIDPATH
+  sleep $INTERVAL
+done
+
